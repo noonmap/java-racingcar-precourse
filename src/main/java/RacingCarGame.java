8afcf7b065cc81ead7222ee1c5ddf5f887ba82ad
Chaewon Jeong <lalae0228@gmail.com>
2021-05-04 20:42:38 +0900

    RacingCarGame 검증

---
+++ src/main/java/RacingCarGame.java
@@ -0,0 +1,35 @@
+package main.java;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RacingCarGame {
+	private Cars cars;
+	private MoveCountNumber moveCount;
+	Scanner scanner;
+	
+	private void createCars() throws IOException{
+		System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉽표(,) 기준으로 구분)");
+		cars = new Cars(scanner.nextLine());
+	}
+	private void createMoveCountNumber() throws IOException{
+		System.out.println("시도할 회수는 몇회인가요?");
+		moveCount = new MoveCountNumber(scanner.nextInt());
+	}
+	public RacingCarGame() throws IOException {
+		scanner = new Scanner(System.in);
+		createCars();
+		createMoveCountNumber();
+		scanner.close();
+	}
+
+	public void playRacingCarGame() {
+		for(int cnt=0; cnt<moveCount.getMoveCount(); cnt++) {
+			cars.play();
+			cars.report();
+		}
+	}
+	public void printResult() {
+		System.out.println( cars.getWinners() + "가 최종 우승했습니다.");
+	}
+}
---
+++ src/test/java/RacingCarGameTest.java
@@ -0,0 +1,26 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import main.java.RacingCarGame;
+
+class RacingCarGameTest {
+
+	@Test
+	@DisplayName("정상적인 입력 검증")
+	void 정상적인_입력_검증() {
+        String input = "aaa,bbb,ccc\n5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertAll(()->{
+            new RacingCarGame();
+        });
+	}
+}
