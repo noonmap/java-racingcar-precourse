20b23efa7df62f43fcf878433ff581156f6a2542
Chaewon Jeong <lalae0228@gmail.com>
2021-05-04 16:29:47 +0900

    MoveCountNumber 수정 및 검증
    지정된 숫자 범위를 이탈했을 때, 반환값 대신 Exception으로 예외처리를 함

--- src/main/java/MoveCountNumber.java
+++ src/main/java/MoveCountNumber.java
@@ -5,10 +5,13 @@ public class MoveCountNumber {
 	private static final int MAX_COUNT = 10;
 	private static int count;
 
-	public MoveCountNumber(int no){
+	public MoveCountNumber(int no) throws RuntimeException{
+		validNo(no);
 		this.count = no;
 	}
-	public boolean validNo() {
-		return this.count >= MIN_COUNT && this.count <= MAX_COUNT;
+	public void validNo(int no) throws RuntimeException{
+		if(no < MIN_COUNT || no > MAX_COUNT) {
+			throw new RuntimeException("이동 횟수는 10회 이내의 숫자여야 합니다.");
+		}
 	}
 }
--- src/test/java/MoveCountNumberTest.java
+++ src/test/java/MoveCountNumberTest.java
@@ -8,13 +8,18 @@ import org.junit.jupiter.api.Test;
 import main.java.MoveCountNumber;
 
 class MoveCountNumberTest {
-
 	@Test
 	@DisplayName("MoveCountNumber 검증")
 	void validateMoveCountNumber() {
-		assertFalse(new MoveCountNumber(0).validNo());
-		assertTrue(new MoveCountNumber(1).validNo());
-		assertTrue(new MoveCountNumber(10).validNo());
-		assertFalse(new MoveCountNumber(11).validNo());
+		assertAll(()->{
+			new MoveCountNumber(1);
+			new MoveCountNumber(10);
+		});
+		assertThrows(RuntimeException.class, ()->{
+			new MoveCountNumber(0);
+		});
+		assertThrows(RuntimeException.class, ()->{
+			new MoveCountNumber(11);
+		});
 	}
 }
